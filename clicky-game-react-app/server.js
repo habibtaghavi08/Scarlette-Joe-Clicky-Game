e1e8101c697662cf62f9e629bc92ffea26ed6db9
habibtaghavi08 <habibtaghavi08@gmail.com>
2020-05-29 00:58:51 -0500

    added routes files and coded them, still working on file structure

--- clicky-game-react-app/server.js
+++ clicky-game-react-app/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
-const mongoose = require("mongoose");
-const routes = require("./routes");
+// const mongoose = require("mongoose");
+const routes = require("../../Clicky-Game/clicky-game-react-app/routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+ 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,13 +14,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+routes(app);
 
 // Connect to the Mongo DB
 // TODO: rename the database to something that will relate to our app
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/GoogleBooksDB");
 
-// Start the API server
+// Start the API server and also last command to create server 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
