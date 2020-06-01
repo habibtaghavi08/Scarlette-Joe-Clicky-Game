61ee38f820dfab6e87adecb332da88bd4873e361
habibtaghavi08 <habibtaghavi08@gmail.com>
2020-06-01 00:29:06 -0500

    Updates

---
+++ precache-manifest.0e49445a1fc69d4f0cd04464cde70a8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a75bc1532eb6a0e8c195f3901047fa",
+    "url": "/Scarlette-Joe-Clicky-Game/index.html"
+  },
+  {
+    "revision": "c337965b135f8c145470",
+    "url": "/Scarlette-Joe-Clicky-Game/static/css/main.8999b105.chunk.css"
+  },
+  {
+    "revision": "ccd7db151b992ddf8e56",
+    "url": "/Scarlette-Joe-Clicky-Game/static/js/2.309b0860.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Scarlette-Joe-Clicky-Game/static/js/2.309b0860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c337965b135f8c145470",
+    "url": "/Scarlette-Joe-Clicky-Game/static/js/main.ffe459ec.chunk.js"
+  },
+  {
+    "revision": "be11b9f9cf4e599468b4",
+    "url": "/Scarlette-Joe-Clicky-Game/static/js/runtime-main.88efcb7a.js"
+  }
+]);
